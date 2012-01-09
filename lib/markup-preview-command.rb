8ea41bdc346cf9bdaa344a0d8e796bb36bc321ab
mori-dev <mori.dev.asdf@gmail.com>
2012-01-09 19:02:10 +0900

    refactor

--- lib/markup-preview-command.rb
+++ lib/markup-preview-command.rb
@@ -3,7 +3,8 @@ require 'markup-preview-command/version'
 require 'markup-preview-command/util/util'
 
 module Markup::Preview::Command
-  class Render
+
+  class Runner
 
     require 'github/markup'
     require 'fileutils'
@@ -15,20 +16,18 @@ module Markup::Preview::Command
     TMPFILE = File.join(TMPDIR, $$.to_s)
     FileUtils.mkdir_p(TMPDIR) unless FileTest.directory? TMPDIR
 
-    def initialize(opts)
-      @content = nil
-      @format  = nil
-      @browser = false
-      parse_options(opts)
+
+    def initialize(argv)
+      @opts = Options.new(argv)
     end
 
-    def self.execute(opts)
-      new(opts).execute
+    def self.run(argv)
+      self.new(argv).run
     end
 
-    def execute
+    def run
       create_html
-      if @browser
+      if @opts.browser
         open_browser
         exit($?.exitstatus || 0)
       else
@@ -41,7 +40,7 @@ module Markup::Preview::Command
     private
 
     def create_html
-      render_result = GitHub::Markup.render("dummy.#{@format}", @content)
+      render_result = GitHub::Markup.render("dummy.#{@opts.format}", @opts.content)
       template_css  = File.read(File.join(File.dirname(__FILE__), 'markup-preview-command', 'css', 'template.css'))
       gollum_css    = File.read(File.join(File.dirname(__FILE__), 'markup-preview-command', 'css', 'gollum.css'))
       html          = ERB.new(File.read(File.join(File.dirname(__FILE__), 'markup-preview-command', 'view', 'template.erb')))
@@ -54,11 +53,45 @@ module Markup::Preview::Command
       system "#{browser_command} #{TMPFILE}"
     end
 
-    def parse_options(opts)
+  end
+
+
+  class Options
+
+    require 'slop'
+
+    attr_reader :format, :browser, :content
+
+    def initialize(argv)
+      parse(argv)
+    end
+
+
+    private
+
+    def parse(argv)
+
+      usage if argv.length == 0
+
+      help = <<-EOF
+useage:
+markup-preview -f path/to/foo.md -o browser
+or,
+cat foo.md | markup-preview -m markdown -o browser
+  EOF
+
+      opts = Slop.parse(argv) do
+        on :h, :help, help, :tail => true do
+          puts help
+          exit
+        end
+        on :m,  :markup=,   'markup'
+        on :f,  :filepath=, 'filepath'
+        on :o,  :output=,   'set "-o browser" if see result by browser'
+      end
 
       extensions = ['md','mkd','mkdn','mkdn','mdown','markdown','org','pod',
                     'creole','rst','rest','textile','rdoc','mediawiki']
-
 
       @format = opts[:markup].downcase if opts.markup?
 
@@ -77,5 +110,12 @@ module Markup::Preview::Command
       @content = opts.filepath? ? File.read(opts[:filepath]) : $stdin.read
 
     end
+
+    def usage
+      puts "Incorrect usage."
+      puts "Check out --help for options"
+      exit 1
+    end
+
   end
 end
