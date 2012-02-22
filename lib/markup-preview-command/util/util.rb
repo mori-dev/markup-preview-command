503655ebfbe6cc034fde5fdc735d7522c53b6d31
Kouhei Yanagita <yanagi@shakenbu.org>
2012-02-22 22:05:59 +0900

    fix spelling error

--- lib/markup-preview-command/util/util.rb
+++ lib/markup-preview-command/util/util.rb
@@ -1,13 +1,13 @@
 module Markup::Preview::Command
   module Util
 
-    def command_exit? (command)
+    def command_exist? (command)
       system "which #{command} >/dev/null 2>&1"
     end
 
     def browser_command
-      return "gnome-open" if command_exit? "gnome-open"
-      return "open" if command_exit? "open"
+      return "gnome-open" if command_exist? "gnome-open"
+      return "open" if command_exist? "open"
       exit 1
     end
 
